961828fd2be1043e4ef38a4dbb9c6556a820b55a
leonidas ioannidis <leonidas@cryptolab.net>
2017-07-20 17:42:25 +0200

    Recursive consulting according to num of paginations

--- index.php
+++ index.php
@@ -9,22 +9,44 @@
   mb_internal_encoding("UTF-8"); //Set encoding to UTF-8 to deal with greek locale
   include './simple_html_dom.php'; //DOM Parser
   $html = file_get_html('https://www.kariera.gr/%CE%AD%CF%81%CE%B5%CF%85%CE%BD%CE%B1?q=%CE%BC%CE%B7%CF%87%CE%B1%CE%BD%CE%B9%CE%BA%CF%8C%CF%82&loc=%CE%91%CE%B8%CE%AE%CE%BD%CE%B1'); //Load page with sample search
+  //Generic leave commented
+  // foreach($html->find('table.jobList_table') as $element){
+  //   echo $element->innertext()."<br>";
+  // }
   //Read number of pages
   foreach($html->find('table.jobList_table div[id=paginationBottom]') as $element) {
     $pages =  $element->plaintext; //Take string containing information of pagination
     $pagelength = mb_substr($pages, 47, 70); // returns the max number page
   }
-  
-  foreach($html->find('table.jobList_table h2') as $element){
-    $item['title'] =  $element->plaintext."<br>";
-    $item['time'] = $element->nextsibling()->plaintext."<br>";
-    $item['body'] = $element->nextsibling(1)->nextsibling(1)->plaintext."<br>";
-    $item['link'] =  $element->nextsibling(1)->nextsibling(1)->nextsibling(1)->childnodes(0)->firstChild(1)->innertext()."<br>";
-    echo $item['title'];
-    echo $item['time'];
-    echo $item['body'];
-    echo $item['link'];
+  for ($i=1; $i <= $pagelength ; $i++) { 
+    if ($i == 1) {
+      $html = file_get_html('https://www.kariera.gr/%CE%AD%CF%81%CE%B5%CF%85%CE%BD%CE%B1?q=%CE%BC%CE%B7%CF%87%CE%B1%CE%BD%CE%B9%CE%BA%CF%8C%CF%82&loc=%CE%91%CE%B8%CE%AE%CE%BD%CE%B1'); //Load page with sample search
+      foreach($html->find('table.jobList_table h2') as $element){
+        $item['title'] =  $element->plaintext."<br>";
+        $item['time'] = $element->nextsibling()->plaintext."<br>";
+        $item['body'] = $element->nextsibling(1)->nextsibling(1)->plaintext."<br>";
+        $item['link'] =  $element->nextsibling(1)->nextsibling(1)->nextsibling(1)->childnodes(0)->firstChild(1)->innertext()."<br>";
+        echo $item['title'];
+        echo $item['time'];
+        echo $item['body'];
+        echo $item['link'];
+      }
+    }
+    else {
+      $html = file_get_html("https://www.kariera.gr/%CE%AD%CF%81%CE%B5%CF%85%CE%BD%CE%B1?q=%CE%BC%CE%B7%CF%87%CE%B1%CE%BD%CE%B9%CE%BA%CF%8C%CF%82&loc=%CE%91%CE%B8%CE%AE%CE%BD%CE%B1&pg=".$i); //Load page with sample search
+      foreach($html->find('table.jobList_table h2') as $element){
+        $item['title'] =  $element->plaintext."<br>";
+        $item['time'] = $element->nextsibling()->plaintext."<br>";
+        $item['body'] = $element->nextsibling(1)->nextsibling(1)->plaintext."<br>";
+        $item['link'] =  $element->nextsibling(1)->nextsibling(1)->nextsibling(1)->childnodes(0)->firstChild(1)->innertext()."<br>";
+        echo $item['title'];
+        echo $item['time'];
+        echo $item['body'];
+        echo $item['link'];
+      }
+    }
   }
+  
 
   $html->clear(); 
   unset($html);
