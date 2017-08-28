8f1b78b8451bc8bce660096d1271bc56627caf0e
leonidas ioannidis <leonidas@cryptolab.net>
2017-08-28 23:13:10 +0200

    issue #5 : Independent search for query location and combined

--- results.php
+++ results.php
@@ -1,11 +1,15 @@
 <?php
+$url = "https://www.kariera.gr/%CE%AD%CF%81%CE%B5%CF%85%CE%BD%CE%B1?q=";
 if (isset($_POST['what']) && !empty($_POST['what'])) {
   $what = $_POST['what'];
+  $url .= $what;
 } 
 if (isset($_POST['where']) && !empty($_POST['where'])) {
   $where = $_POST['where'];
+  $url .= "&loc=".$where;
 }
-$url = "https://www.kariera.gr/%CE%AD%CF%81%CE%B5%CF%85%CE%BD%CE%B1?q=".$what."&loc=".$where."&rpp=50"
+$url .= "&rpp=50";
+echo $url;
 ?>
 <!DOCTYPE html>
 <html>
@@ -44,53 +48,50 @@ $url = "https://www.kariera.gr/%CE%AD%CF%81%CE%B5%CF%85%CE%BD%CE%B1?q=".$what."&
   mb_internal_encoding("UTF-8"); //Set encoding to UTF-8 to deal with greek locale
   include './simple_html_dom.php'; //DOM Parser
   error_reporting(0); //Turn off errors
-  if (file_get_contents($url)) {
-  error_reporting(E_ERROR | E_WARNING | E_PARSE); //Restore error reporting
-  $html = file_get_html($url); //Load page with sample search
-  //Generic leave commented
-  // foreach($html->find('table.jobList_table') as $element){
-  //   echo $element->innertext()."<br>";
-  // }
-  // break;
-  
-   
-  
-  
-  //Read number of pages
-  foreach($html->find('table.jobList_table div[id=paginationBottom]') as $element) {
-    $pages =  $element->plaintext; //Take string containing information of pagination
-    $pagelength = mb_substr($pages, 47, 70); // returns the max number page
-    //echo $pagelength;
-  }
+  if (file_get_contents($url)) { //check if there is response from site
+    error_reporting(E_ERROR | E_WARNING | E_PARSE); //Restore error reporting
+    $html = file_get_html($url); //Load page with sample search
+    //Generic leave commented
+    // foreach($html->find('table.jobList_table') as $element){
+    //   echo $element->innertext()."<br>";
+    // }
+    // break;
+    
+    //Read number of pages
+    foreach($html->find('table.jobList_table div[id=paginationBottom]') as $element) {
+      $pages =  $element->plaintext; //Take string containing information of pagination
+      $pagelength = mb_substr($pages, 47, 70); // returns the max number page
+      //echo $pagelength;
+    }
 
-  $html = file_get_html($url); //Load page with sample search
-  foreach($html->find('table.jobList_table h2') as $element){
-    echo "<div class='w3-panel w3-leftbar w3-hover-border-green w3-padding w3-animate-opacity'>";
-    $item['title'] =  $element->plaintext."<br>";
-    echo "<div class='title'>".$item['title']."</div>";
-    $item['time'] = $element->nextsibling()->plaintext."<br>";
-    echo "<div class='time'>".$item['time']."</div>";
-    $item['link'] =  $element->nextsibling(1)->nextsibling(1)->nextsibling(1)->childnodes(0)->firstChild(1)->firstChild(1)->firstChild(1)->childNodes(1)->outertext()."<br>";
-    echo "<div class='link'>".$item['link']."</div></div>";
-    flush();
-    ob_flush();
-    usleep(200000);
-  }
-
-  for ($i=2; $i <= $pagelength ; $i++) { 
-    $html = file_get_html($url."&pg=".$i); //Load page with sample search
+    $html = file_get_html($url); //Load page with sample search
     foreach($html->find('table.jobList_table h2') as $element){
-      echo "<div class='w3-panel w3-leftbar w3-hover-border-green w3-padding'>";
+      echo "<div class='w3-panel w3-leftbar w3-hover-border-green w3-padding w3-animate-opacity'>";
       $item['title'] =  $element->plaintext."<br>";
       echo "<div class='title'>".$item['title']."</div>";
       $item['time'] = $element->nextsibling()->plaintext."<br>";
       echo "<div class='time'>".$item['time']."</div>";
       $item['link'] =  $element->nextsibling(1)->nextsibling(1)->nextsibling(1)->childnodes(0)->firstChild(1)->firstChild(1)->firstChild(1)->childNodes(1)->outertext()."<br>";
       echo "<div class='link'>".$item['link']."</div></div>";
+      flush();
+      ob_flush();
+      usleep(200000);
     }
-  }
-  $html->clear(); 
-  unset($html);
+
+    for ($i=2; $i <= $pagelength ; $i++) { 
+      $html = file_get_html($url."&pg=".$i); //Load page with sample search
+      foreach($html->find('table.jobList_table h2') as $element){
+        echo "<div class='w3-panel w3-leftbar w3-hover-border-green w3-padding'>";
+        $item['title'] =  $element->plaintext."<br>";
+        echo "<div class='title'>".$item['title']."</div>";
+        $item['time'] = $element->nextsibling()->plaintext."<br>";
+        echo "<div class='time'>".$item['time']."</div>";
+        $item['link'] =  $element->nextsibling(1)->nextsibling(1)->nextsibling(1)->childnodes(0)->firstChild(1)->firstChild(1)->firstChild(1)->childNodes(1)->outertext()."<br>";
+        echo "<div class='link'>".$item['link']."</div></div>";
+      }
+    }
+    $html->clear(); 
+    unset($html);
   }
   echo "Nothing to show";
   ?>
